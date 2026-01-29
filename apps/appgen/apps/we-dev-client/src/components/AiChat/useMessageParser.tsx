cb0e39012be003c0dd6f6a6575f74cfb3b10ce8a
arolleaguekeng <arolleaguekeng@gmail.com>
2026-01-29 15:16:45 +0100

    Generation ok

--- apps/appgen/apps/we-dev-client/src/components/AiChat/useMessageParser.tsx
+++ apps/appgen/apps/we-dev-client/src/components/AiChat/useMessageParser.tsx
@@ -1,9 +1,8 @@
-import { FileAction, StreamingMessageParser } from "./messae";
+import { FileAction, StreamingMessageParser } from './messae';
 
-
-import { createFileWithContent } from "../WeIde/components/IDEContent/FileExplorer/utils/fileSystem";
-import useTerminalStore from "@/stores/terminalSlice";
-import { Message } from "ai/react";
+import { createFileWithContent } from '../WeIde/components/IDEContent/FileExplorer/utils/fileSystem';
+import useTerminalStore from '@/stores/terminalSlice';
+import { Message } from 'ai/react';
 
 class Queue {
   private queue: string[] = [];
@@ -31,7 +30,7 @@ class Queue {
       while (this.queue.length > 0) {
         const command = this.getNext();
         if (command) {
-          console.log("执行命令", command);
+          console.log('执行命令', command);
           await useTerminalStore.getState().getTerminal(0).executeCommand(command);
         }
       }
@@ -43,14 +42,13 @@ class Queue {
 
 export const queue = new Queue();
 
-
 class List {
   private isRunArray: string[] = [];
   private nowArray: string[] = [];
 
   // 添加命令到队列
   run(commands: string[]) {
-    this.nowArray = commands
+    this.nowArray = commands;
     this.process();
   }
 
@@ -65,12 +63,12 @@ class List {
 
   // 处理队列
   private async process() {
-    console.log("this.nowArray", this.nowArray, this.isRunArray);
+    console.log('this.nowArray', this.nowArray, this.isRunArray);
     for (let i = 0; i < this.nowArray.length; i++) {
       const command = this.getCommand(i);
       const isRuned = this.getIsRun(i);
       if (command && command !== isRuned) {
-        console.log("执行命令", command);
+        console.log('执行命令', command);
         this.isRunArray[i] = command;
         queue.push(command);
       }
@@ -89,18 +87,17 @@ export const execList = new List();
 const messageParser = new StreamingMessageParser({
   callbacks: {
     onActionStream: async (data) => {
-       createFileWithContent((data.action as FileAction).filePath, data.action.content, true);
+      createFileWithContent((data.action as FileAction).filePath, data.action.content, true);
       //   workbenchStore.runAction(data, true);
     },
   },
 });
 
-
 export const parseMessages = async (messages: Message[]) => {
   for (let i = 0; i < messages.length; i++) {
     const message = messages[i];
-    if (message.role === "assistant") {
+    if (message.role === 'assistant') {
       messageParser.parse(message.id, message.content);
     }
   }
-}
+};
