21c4196e6442c62c6938ba303c96b08f5e7a06bd
Romuald <romuald@idem.ai>
2026-04-14 22:36:55 +0100

    fix styles problems

--- apps/ideploy/vite.config.js
+++ apps/ideploy/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
